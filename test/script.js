81997d8046197bc6738d6af1939cfac2404f6a0f
Niyati3009 <106224310+Niyati3009@users.noreply.github.com>
2022-06-06 16:05:11 +0530

    Create script.js

---
+++ test/script.js
@@ -0,0 +1,78 @@
+'use strict';
+
+const AdminConnection = require('composer-admin').AdminConnection;
+const BusinessNetworkConnection = require('composer-client').BusinessNetworkConnection;
+const { BusinessNetworkDefinition, CertificateUtil, IdCard } = require('composer-common');
+const path = require('path');
+
+const chai = require('chai');
+chai.should();
+chai.use(require('chai-as-promised'));
+
+const namespace = 'org.supplychain.network';
+const orderId = '1000-1000-1000-1000';
+const vin = '1a2b3c4d5e6f7g8h9';
+
+describe('Manufacture network', () => {
+    const cardStore = require('composer-common').NetworkCardStoreManager.getCardStore( { type: 'composer-wallet-inmemory' } );
+    let adminConnection;
+    let businessNetworkConnection;
+    let factory;
+
+    before(async () => {
+        // Embedded connection used for local testing
+        const connectionProfile = {
+            name: 'embedded',
+            'x-type': 'embedded'
+        };
+        // Generate certificates for use with the embedded connection
+        const credentials = CertificateUtil.generate({ commonName: 'admin' });
+
+        // PeerAdmin identity used with the admin connection to deploy business networks
+        const deployerMetadata = {
+            version: 1,
+            userName: 'PeerAdmin',
+            roles: [ 'PeerAdmin', 'ChannelAdmin' ]
+        };
+
+        const deployerCard = new IdCard(deployerMetadata, connectionProfile);
+        deployerCard.setCredentials(credentials);
+
+        const deployerCardName = 'PeerAdmin';
+        adminConnection = new AdminConnection({ cardStore: cardStore });
+
+        await adminConnection.importCard(deployerCardName, deployerCard);
+        await adminConnection.connect(deployerCardName);
+    });
+
+    beforeEach(async () => {
+        businessNetworkConnection = new BusinessNetworkConnection({ cardStore: cardStore });
+
+        const adminUserName = 'admin';
+        let adminCardName;
+        const businessNetworkDefinition = await BusinessNetworkDefinition.fromDirectory(path.resolve(__dirname, '..'));
+
+        // Install the Composer runtime for the new business network
+        await adminConnection.install(businessNetworkDefinition);
+
+        // Start the business network and configure an network admin identity
+        const startOptions = {
+            networkAdmins: [
+                {
+                    userName: adminUserName,
+                    enrollmentSecret: 'adminpw'
+                }
+            ]
+        };
+        const adminCards = await adminConnection.start(businessNetworkDefinition.getName(), businessNetworkDefinition.getVersion(), startOptions);
+
+        // Import the network admin identity for us to use
+        adminCardName = `${adminUserName}@${businessNetworkDefinition.getName()}`;
+
+        await adminConnection.importCard(adminCardName, adminCards.get(adminUserName));
+
+        // Connect to the business network using the network admin identity
+        await businessNetworkConnection.connect(adminCardName);
+
+        factory = businessNetworkConnection.getBusinessNetwork().getFactory();
+    });
